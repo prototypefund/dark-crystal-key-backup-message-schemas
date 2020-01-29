5604a47ec7d7be7e91052b135ef7eb2f47e2ebd9
ameba23 <ameba23@systemli.org>
2020-01-29 15:41:57 +0100

    update shard schema

--- schemas/shard/v1.js
+++ schemas/shard/v1.js
@@ -1,24 +1,18 @@
+const basics = require('../basics')
+const shard = Object.assign(basics.hexString(), { minlength: basics.MINSHARDLENGTHBYTES * 2 })
+
 module.exports = {
   $schema: 'http://json-schema.org/schema#',
   type: 'object',
-  required: ['type', 'version', 'root', 'shard', 'recipient'],
+  required: ['type', 'version', 'root', 'shard', 'recipient', 'timestamp'],
   properties: {
-    type: {
-      type: 'string',
-      pattern: '^dark-crystal/shard$'
-    },
-    version: {
-      type: 'string',
-      pattern: '^1.0.0$'
-    },
-    root: {
-      // TODO
-    },
-    shard: {
-      // TODO
-    },
-    recipient: {
-      // TODO (publickey)
+    type: basics.type('shard'),
+    version: basics.version('1.0.0'),
+    root: basics.messageRef,
+    shard,
+    recipient: basics.hexString(basics.PUBLICKEYLENGTH),
+    timestamp: {
+      type: 'integer'
     }
   }
 }
