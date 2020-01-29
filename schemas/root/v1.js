de83f9d03a3ef08e7a370eee8fcc9d394b2f5997
ameba23 <ameba23@systemli.org>
2020-01-29 15:41:43 +0100

    update root schema

--- schemas/root/v1.js
+++ schemas/root/v1.js
@@ -1,16 +1,12 @@
+const basics = require('../basics')
+
 module.exports = {
   $schema: 'http://json-schema.org/schema#',
   type: 'object',
-  required: ['type', 'version', 'name', 'quorum', 'shards'],
+  required: ['type', 'version', 'name', 'quorum', 'shards', 'timestamp'],
   properties: {
-    type: {
-      type: 'string',
-      pattern: '^dark-crystal/root$'
-    },
-    version: {
-      type: 'string',
-      pattern: '^1.0.0$'
-    },
+    type: basics.type('root'),
+    version: basics.version('1.0.0'),
     name: { type: 'string' },
     quorum: {
       type: 'integer',
@@ -20,6 +16,7 @@ module.exports = {
       type: 'integer',
       minimum: 2
     },
-    tool: { type: 'string' }
+    tool: { type: 'string' },
+    timestamp: { type: 'integer' }
   }
 }
