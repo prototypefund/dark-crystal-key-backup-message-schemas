c9db5c963b0f384e067bea774a9d78bbc91adbdb
ameba23 <ameba23@systemli.org>
2020-01-29 18:42:54 +0100

    fix forward schema

--- schemas/forward/v1.js
+++ schemas/forward/v1.js
@@ -1,12 +1,12 @@
 const basics = require('../basics')
-const shard = Object.assign(basics.hexString(), { minlength: basics.MINSHARDLENGTHBYTES * 2 })
+const shard = Object.assign(basics.hexString(), { minLength: basics.MINSHARDLENGTHBYTES * 2 })
 
 module.exports = {
   $schema: 'http://json-schema.org/schema#',
   type: 'object',
   required: ['type', 'version', 'root', 'shard', 'recipient', 'timestamp'],
   properties: {
-    type: basics.type('reply'),
+    type: basics.type('forward'),
     version: basics.version('1.0.0'),
     root: basics.messageRef,
     shard,
